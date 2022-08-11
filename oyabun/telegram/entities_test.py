aa229853d7b155cb769f6cb638b3c5402a4fac1c
Alexander Sidorov <alexander@sidorov.dev>
2022-08-12 02:23:08 +0300

    5

--- oyabun/telegram/entities_test.py
+++ oyabun/telegram/entities_test.py
@@ -5,7 +5,7 @@ from oyabun.telegram import Message
 from oyabun.telegram import User
 
 
-def test_update_message_from() -> None:
+def test_update_message_from_1() -> None:
     msg = Message(
         message_id=1,
         date="2020-10-23T13:12:11Z",  # type: ignore
@@ -29,3 +29,25 @@ def test_update_message_from() -> None:
     assert msg.from_.id == 1
     assert msg.from_.is_bot is True
     assert msg.from_.first_name == "fn"
+
+
+def test_update_message_from_2() -> None:
+    user = User(id=1, is_bot=True, first_name="fn")
+
+    msg1 = Message(
+        message_id=1,
+        date="2020-10-23T13:12:11Z",  # type: ignore
+        chat=Chat(id=1),
+        from_=user,
+    )
+    msg2 = Message(
+        message_id=2,
+        date="2020-01-02T03:04:05Z",  # type: ignore
+        chat=Chat(id=1),
+        **{"from": user},  # type: ignore
+    )
+
+    d1 = msg1.dict()
+    d2 = msg2.dict()
+
+    assert d1["from"] == d2["from"]
