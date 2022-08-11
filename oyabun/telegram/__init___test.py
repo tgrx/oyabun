83bcfc3c0af11d45f560ccdb4073f274b5c80675
Alexander Sidorov <alexander@sidorov.dev>
2022-08-12 01:55:37 +0300

    10

---
+++ oyabun/telegram/__init___test.py
@@ -0,0 +1,9 @@
+from .base import TelegramBotApiType
+
+
+def test_photosize_importable_from_pkg() -> None:
+    from oyabun import telegram
+
+    obj = getattr(telegram, "PhotoSize", None)
+    assert obj is not None
+    assert issubclass(obj, TelegramBotApiType)
