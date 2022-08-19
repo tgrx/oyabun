31000cd4d27ece73be0141d146d93ecd9c317232
Alexander Sidorov <alexander@sidorov.dev>
2022-08-20 02:42:15 +0300

    5

--- oyabun/version_test.py
+++ oyabun/version_test.py
@@ -3,7 +3,7 @@ from oyabun.version import verify_version
 
 
 def test_version() -> None:
-    assert oyabun.__version__ == "2022.9.1"
+    assert oyabun.__version__ == "2022.8.20"
 
 
 def test_verify_version() -> None:
