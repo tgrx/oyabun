62659d6b5259cefe91781e78fd51113d33eff8c9
Alexander Sidorov <alexander@sidorov.dev>
2022-12-10 21:11:29 +0300

    fixed ci (2)

--- tests/version_test.py
+++ tests/version_test.py
@@ -3,7 +3,7 @@ from oyabun.version import verify_version
 
 
 def test_version() -> None:
-    assert oyabun.__version__ == "2022.8.20"
+    assert oyabun.__version__ == "2022.12.12"
 
 
 def test_verify_version() -> None:
